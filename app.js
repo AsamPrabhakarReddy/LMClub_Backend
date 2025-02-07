bf3174413fb83633b01d784bf466d73a3d8a8307
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-02-07 17:58:05 +0530

    updated cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: ['https://lmclub.vercel.app/']
+  origin: ['https://lmclub.vercel.app']
 }));
 app.use(cookieParser());
 app.use(bodyParser.json());
