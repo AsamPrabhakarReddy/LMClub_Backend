abb47402d63480614fd9e8c3652dfffa58e041dc
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-02-19 16:39:12 +0530

    added more timout session

--- Confiq/DB.js
+++ Confiq/DB.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URL);
+      await mongoose.connect(process.env.MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, 
+        socketTimeoutMS: 30000,
+      });
       console.log(`MongoDB connected ${mongoose.connection.host}`);
     } catch (error) {
       console.log(`Mongo DB server issue ${error}`);
