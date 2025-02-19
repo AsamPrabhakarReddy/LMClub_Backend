1394c44f73638aed260d468086400ffbb8d9243a
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-02-19 17:02:53 +0530

    added mongoDB url directly

--- Confiq/DB.js
+++ Confiq/DB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URL,{
+      await mongoose.connect("mongodb+srv://admin:cdnlmclub@cdn-lmclub.snbxd.mongodb.net/?retryWrites=true&w=majority&appName=CDN-LMClub",{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 30000, 
